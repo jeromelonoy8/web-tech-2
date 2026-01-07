2b4ebf6e270395cf9fcaa27ea9829a34824c7c7f
Heaven <hcortez44359@liceo.edu.ph>
2026-01-07 12:57:43 +0800

    prelim exam submission

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { Activity1Component } from './activity/activity-1.component';
+import { PrelimExamComponent } from './prelim-exam/prelim-exam.component';
+
+
 export const routes: Routes = [
     {
         path: 'activity-1',
         component: Activity1Component
+    },
+    {
+        path: 'prelim-exam',
+        component: PrelimExamComponent
     }
 ];
